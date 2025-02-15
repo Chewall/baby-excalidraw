200998c59bad81080aef1d764764fc9ef279f9a0
hardyX <1148140773@qq.com>
2025-02-15 22:58:10 +0800

    尝试画图

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
+import {useLayoutEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 function App() {
 
+  useLayoutEffect(() => {
+    const canvas = document.getElementById('canvas')
+    const ctx = canvas.getContext('2d')
+
+    ctx.fillStyle = 'green'
+    ctx.fillRect(10, 10, 150, 100)
+    ctx.strokeRect(200, 200, 100, 100)
+  }, []);
+
   return (
     <canvas
       id='canvas'
-      style={{ backgroundColor: 'black' }}
       width={window.innerWidth}
       height={window.innerHeight}
     >

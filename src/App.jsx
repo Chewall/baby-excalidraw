e27773cdc311be1edb32d362db50f33e93204751
hardyX <1148140773@qq.com>
2025-02-16 11:04:45 +0800

    测试鼠标按住移动是否改变坐标

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ const generator = rough.generator()
 
 function App() {
 
+  const [elements, setElements] = useState([])
+  const [drawing, setDrawing] = useState(false)
+
   useLayoutEffect(() => {
     const canvas = document.getElementById('canvas')
     const ctx = canvas.getContext('2d')
@@ -14,11 +17,28 @@ function App() {
     roughCanvas.draw(rect)
   }, []);
 
+  const handleMouseDown = (event) => {
+    setDrawing(true)
+  }
+  
+  const handleMouseMove = (event) => {
+    if (!drawing) return
+    
+    const { clientX, clientY } = event
+    console.log(clientX, clientY)
+  } 
+  
+  const handleMouseUp = () => {
+    setDrawing(false)
+  }
   return (
     <canvas
       id='canvas'
       width={window.innerWidth}
       height={window.innerHeight}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
     >
       canvas
     </canvas>
